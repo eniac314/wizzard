5bf197fe1d61459af69ce12cbea93678fcc3e1fd
fayong <eniac314@gmail.com>
2015-04-25 16:43:59 +0200

    replaced custom fps limiter with sdl default one

--- tiler.hs
+++ tiler.hs
@@ -1,6 +1,8 @@
+import Control.Monad
 import qualified Graphics.UI.SDL as SDL
 import qualified Graphics.UI.SDL.Primitives as SDLP
 import qualified Graphics.UI.SDL.Image as SDLI
+import qualified Graphics.UI.SDL.Framerate as SDLF
 import GHC.Word
 import GHC.Int
 import Data.Bits
@@ -40,6 +42,18 @@ data LandType = Swamp
 
 data Tile = Water WaterType 
           | Land LandType deriving Show
+
+--------------------------------------------------------------------------------------------------------
+{- misc helper functions-}
+
+osc :: Int -> Int -> Int -> [Int]
+osc a b c = cycle $ [a,a+c..b] ++ [b,b-c..a+c]
+
+slow :: Int -> [a] -> [a]
+slow 0 xs = xs
+slow n [] = []
+slow n (x:xs) = go n x [] ++ (slow n xs) where go 0 _ xs = xs
+                                               go n x xs = go (n-1) x (x:xs)
 
 -------------------------------------------------------------------------------------------------
 {- Graphics -}
@@ -82,14 +96,14 @@ getTile t =
 	                      _              -> (0,7)	                      
 	in Just SDL.Rect { SDL.rectX = j * 32, SDL.rectY = i * 32 , SDL.rectW = 32, SDL.rectH = 32}
 
-noise2Tile n | n == 0 = Water Deep2
-             | n == 1 = Water Shallow
-             | n == 2 = Land Swamp
-             | n == 3 = Land GrassLand
-             | n == 4 = Land SmallTrees
-             | n == 5 = Land Forest
-             | n == 6 = Land LowMountain
-             | n == 7 = Land HighMountain1
+noise2Tile n | n == 0 = cycle $ slow 4 [Water Deep2,Water Shallow]
+             | n == 1 = cycle $ slow 4 [Water Deep2,Water Shallow]
+             | n == 2 = cycle [Land Swamp]
+             | n == 3 = cycle [Land GrassLand]
+             | n == 4 = cycle [Land SmallTrees]
+             | n == 5 = cycle [Land Forest]
+             | n == 6 = cycle [Land LowMountain]
+             | n == 7 = cycle [Land HighMountain1]
 
 
 applyTile :: Tile -> (Int,Int) -> SDL.Surface -> SDL.Surface -> IO Bool
@@ -98,14 +112,15 @@ applyTile tile (x,y) src dest =
         tileRect = getTile tile
     in SDL.blitSurface src tileRect dest offset
 
-applyTileMat :: Mat Tile -> SDL.Surface -> SDL.Surface -> IO [Bool]
+applyTileMat :: Mat [Tile] -> SDL.Surface -> SDL.Surface -> IO (Mat [Tile])
 applyTileMat m src dest = 
-	let wid = Vec.length (m Vec.! 0) - 1
-	    hei = (Vec.length m) - 1 in
-	sequence $ [ applyTile (m § (i,j)) (32*j, 32*i) src dest | i <- [0..hei], j <- [0..wid]]
+  let wid = Vec.length (m Vec.! 0) - 1
+      hei = (Vec.length m) - 1 in
+  do sequence $ [ applyTile (head (m § (i,j))) (32*j, 32*i) src dest | i <- [0..hei], j <- [0..wid]]
+     return $ fromMat [[(tail (m § (i,j))) | j <- [0..wid]] | i <- [0..hei]]
 
-tileList :: [Mat Tile] -> SDL.Surface -> SDL.Surface -> IO [Mat Tile]
-tileList (t:ts) src dest = applyTileMat t src dest >> return ts
+tileList :: Mat [Tile] -> SDL.Surface -> SDL.Surface -> IO (Mat [Tile])
+tileList m src dest = applyTileMat m src dest >>= (\m' -> return m')
 
 -----------------------------------------------------------------------------------------------------
 {- Vectors -}
@@ -136,40 +151,40 @@ matMap f m = let wid = Vec.length (m Vec.! 0) - 1
 {- Main -}
 
 
-
-
-
-
+delay start stop = let res = 30 - (stop - start)
+                   in if (res > 0 && res < 100 ) then res else 0
 
 width = 1368
 height = 960
 
 main = SDL.withInit [SDL.InitEverything] $ do
+       
+       fpsm <- SDLF.new
+       SDLF.init fpsm
+       
        gen <- getStdGen
        
        let (seed,_) = random gen
-           land = matMap noise2Tile (noiseMat 18 0.2 200 150 7 seed)
+           land = matMap noise2Tile (noiseMat 18 0.2 30 25 7 seed)
        
        args <- getArgs
        
        screen <- SDL.setVideoMode width height 32 [SDL.SWSurface]
        tiles <- loadImage "bigAlphaTiles.png"
        
-       let landList = cycle [land]
-           world = (landList, tiles, screen)
+       let world = (land, tiles, screen, fpsm)
 
-       --putStrLn $ (printVec land)
-       
-
-       let loop w = do SDL.delay 10
-                       let (ls,t,s) = w
+       let loop w = do let (l,t,s,fpsm) = w
                        SDLP.filledPolygon screen [(0,0),(1368,0),(1368,960),(0,960)] (getPixel 0 0 0)
-                       ls' <- tileList ls t s
+                       l' <- tileList l t s
                        SDL.flip s
+                       
        	               event      <- SDL.pollEvent
+                       SDLF.delay fpsm
+                       
                        case event of
                             SDL.Quit -> return ()
                             SDL.KeyDown (SDL.Keysym SDL.SDLK_ESCAPE _ _) -> return ()
-                            SDL.NoEvent -> loop (ls',t,s) 
+                            SDL.NoEvent -> loop (l',t,s,fpsm) 
                             _       -> loop w
        loop world
